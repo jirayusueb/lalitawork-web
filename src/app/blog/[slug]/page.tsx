16cd819a008994ea476108e1ca76574dbcfdfb8a
jirayu(artid) <jirayu.sueb@gmail.com>
2024-11-23 10:14:31 +0700

    refactor: .

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,13 +1,37 @@
 import directus from '@/lib/directus';
-import { createPage } from '@/utils/create-page';
 import { readItems } from '@directus/sdk';
 import { notFound } from 'next/navigation';
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
-import { z } from 'zod';
+import type { Metadata } from 'next';
 
-const { Page } = createPage({
-  component: ({ data }) => (
+type Params = Promise<{ slug: string }>;
+
+interface PageProps {
+  params: Params;
+}
+
+async function getData(slug: string) {
+  const response = await directus?.request(
+    readItems('post', {
+      filter: {
+        slug,
+      },
+    }),
+  );
+
+  if (!response?.length) {
+    notFound();
+  }
+
+  return response[0];
+}
+
+export default async function BlogPost(props: PageProps) {
+  const params = await props.params;
+  const data = await getData(params.slug);
+
+  return (
     <div className="min-h-screen flex flex-col gap-6">
       <div className="flex justify-center items-center min-h-[25vh] border-b py-10">
         <div className="flex flex-col gap-2 text-center max-w-screen-md text-balance">
@@ -21,32 +45,11 @@ const { Page } = createPage({
         </Markdown>
       </div>
     </div>
-  ),
-  loader: async ({ params }) => {
-    const response = await directus?.request(
-      readItems('post', {
-        filter: {
-          slug: params.slug,
-        },
-      }),
-    );
+  );
+}
 
-    if (!response?.length) {
-      notFound();
-    }
-
-    return response[0];
-  },
-  params: z.object({
-    slug: z.string(),
-  }),
-});
-
-export async function generateMetadata({
-  params,
-}: {
-  params: { slug: string };
-}) {
+export async function generateMetadata(props: PageProps): Promise<Metadata> {
+  const params = await props.params;
   const response = await directus?.request(
     readItems('post', {
       filter: {
@@ -55,7 +58,12 @@ export async function generateMetadata({
     }),
   );
 
-  if (!response?.length) return;
+  if (!response?.length) {
+    return {
+      title: 'Not Found',
+      description: 'The requested blog post could not be found',
+    };
+  }
 
   return {
     title: response[0]?.title,
@@ -68,4 +76,3 @@ export async function generateMetadata({
 }
 
 export const revalidate = 3600; // 1 hour
-export default Page;
